5394931122ebbb2d646d95c661f7e7dd6088d3a3
KuuminKochi <yesimkurisu@gmail.com>
2023-11-12 10:36:54 +0800

    website up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,19 @@
 <script lang="ts">
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 style="text-align: center;">Welcome to Kuumin's webpage I suppose</h1>
+
+<div class="main_content">
+	<p>My dumbass kept trying to fix it not knowing I didn't write any code for this at all<br></p>
+</div>
+	<img class="image" src="https://i.pinimg.com/736x/af/0e/4e/af0e4ea3fe63288cb21bc0de408b2739.jpg" alt="">
+<style>
+	.main_content {
+		display: flex;
+		justify-content: center;
+	}
+
+	.image {
+		margin: 0 auto;
+	}
+</style>
