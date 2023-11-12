a17a503ca9f8d53b27619468d8ce4ee50d7c05a6
KuuminKochi <yesimkurisu@gmail.com>
2023-11-12 21:22:13 +0800

    slight update to ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,70 @@
 <script lang="ts">
 </script>
 
-<h1 style="text-align: center;">Welcome to Kuumin's webpage I suppose</h1>
+	<head>
+		<title>kuumin's webpage</title>
+	</head>
 
-<div class="main_content">
-	<p>My dumbass kept trying to fix it not knowing I didn't write any code for this at all<br></p>
-</div>
-	<img class="image" src="https://i.pinimg.com/736x/af/0e/4e/af0e4ea3fe63288cb21bc0de408b2739.jpg" alt="">
+	<body>
+
+		<h1 style="text-align: center;">welcome to my webpage</h1>
+	<div class="container">
+		<img class="img" src="https://i.pinimg.com/736x/af/0e/4e/af0e4ea3fe63288cb21bc0de408b2739.jpg" alt="">
+		<div class="image-next">
+			<p>As you can see, I am quite terrible at webdesign</p>
+			<p>I have no idea what the fuck I'm doing right now<br> but it looks like it is working</p>
+			<p>It's decently minimalistic on desktop as well</p>
+			<p>I kinda like it</p>
+			<p>In a way, it's kinda aesthetic considering <br> I literally have no idea what I'm doing</p>
+			<p>Looks terrible on mobile though</p>
+		</div>
+	</div>
+	</body>
+
 <style>
+	@font-face {
+	  font-family: 'ArcadeClassic';
+	  src: url('/fonts/ArcadeClassic.ttf') format('truetype');
+	  font-weight: normal;
+	  font-style: normal;
+	}
+	
+	html, body {
+		background-color: black;
+		color: white;
+	}
+
 	.main_content {
 		display: flex;
 		justify-content: center;
 	}
 
-	.image {
-		margin: 0 auto;
+	@media screen and (max-width: 600px){
+		.img {
+			display: block;
+			max-width: 80%;
+			height: auto;
+			margin-left: auto;
+			margin-right: auto;
+		}
+
+		.image-next {
+			text-align: center;
+		}
+	}
+	@media screen and (min-width: 601px){
+		.img {
+			float: right;
+			max-width: 50%;
+			height: auto;
+			margin-right: 10%;
+		}
+		
+		.image-next {
+			display: inline-block;
+			text-align: right;
+			margin-left: 500px;
+			margin-top: 50px;
+		}
 	}
 </style>
